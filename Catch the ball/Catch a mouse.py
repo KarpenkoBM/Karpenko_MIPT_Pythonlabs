e991c2478d89e1659e2c55212569275f53f83008
KarpenkoBM <karpenko.bm@phystech.edu>
2021-11-30 18:29:46 +0300

    Added record_table

--- Catch the ball/Catch a mouse.py
+++ Catch the ball/Catch a mouse.py
@@ -7,11 +7,11 @@ import json
 pg.init()
 
 FPS = 30
-size_x, size_y = 800, 800
+size_x, size_y = 800, 800  # задаем размеры экрана
 screen = pg.display.set_mode((size_x, size_y))
-number_of_rodents = 18
-rodents = list()
-font = pg.freetype.SysFont('Times New Roman', 35)
+number_of_rodents = 18  # кол-во грызунов
+rodents = list()  # создаем лист с грызунами
+font = pg.freetype.SysFont('Times New Roman', 35)  # задаем параметры шрифта
 points = 0
 k = 0
 pop_x = 0
@@ -20,11 +20,13 @@ table = False
 timer = 0
 seconds = 30
 minutes = 0
-catpoints = 0
-n = 0
+n = 0   # счетчик для числа очков для грызуна
 
 
 def sign(x):
+    """
+    Функция, возвращающая знак полученной переменной
+    """
     if x >= 0:
         return 1
     else:
@@ -32,7 +34,13 @@ def sign(x):
 
 
 class Mouse:
+    """
+    Создание класса мышей
+    """
     def __init__(self, x1, x2, y1, y2):
+        """
+        Вводим диапозон генерации мышей, их скорость, размер и цвет
+        """
         self.x = randint(x1, x2)
         self.y = randint(y1, y2)
         self.r = randint(10, 20)
@@ -42,6 +50,9 @@ class Mouse:
         self.color = (153, 153, 153)
 
     def move(self, t):
+        """
+        Функция движения мышей
+        """
         if (self.vx == 0) and (self.vx == 0):
             self.vy = randint(-self.speed, self.speed)
             self.vx = randint(-self.speed, self.speed)
@@ -50,6 +61,9 @@ class Mouse:
         self.y += self.vy * t
 
     def reflection(self):
+        """
+        Функция отражения мышей от стен
+        """
         if (self.x + self.vx + self.r >= 700) or \
                 (self.x + self.vx <= 100):
             k = randint(1, self.speed)
@@ -64,17 +78,27 @@ class Mouse:
                               min(abs(self.speed), 700 - self.r - self.x))
 
     def draw_mouse(self):
+        """
+        Функция рисования мыши
+        """
         circle(screen, self.color, (self.x, self.y), self.r)
-        circle(screen, self.color, (self.x + 0.75 * self.r * sign(self.vx), self.y + 0.75 * self.r * sign(self.vy)),
+        circle(screen, self.color, (self.x + 0.75 * self.r * sign(self.vx),
+                                    self.y + 0.75 * self.r * sign(self.vy)),
                self.r * 0.5)
-        circle(screen, (0, 0, 0), (self.x + 1.10 * self.r * sign(self.vx), self.y + 1.10 * self.r * sign(self.vy)),
+        circle(screen, (0, 0, 0), (self.x + 1.10 * self.r * sign(self.vx),
+                                   self.y + 1.10 * self.r * sign(self.vy)),
                self.r * 0.2)
         line(screen, self.color, (self.x, self.y),
-             (self.x - 1.5 * sign(self.vx) * self.r, self.y - 1.5 * self.r * sign(self.vy)), 3)
+             (self.x - 1.5 * sign(self.vx) * self.r,
+              self.y - 1.5 * self.r * sign(self.vy)), 3)
 
     def touch_mouse(self, i):
+        """
+        Функция лопания мышей
+        """
         global points, k, pop_x, pop_y, n
-        if (self.x - event.pos[0]) ** 2 + (self.y - event.pos[1]) ** 2 <= self.r ** 2:
+        if (self.x - event.pos[0]) ** 2 + \
+                (self.y - event.pos[1]) ** 2 <= self.r ** 2:
             rodents.pop(i)
             pop_x = self.x
             pop_y = self.y
@@ -84,7 +108,15 @@ class Mouse:
 
 
 class Rat(Mouse):
+    """
+    Создаем класс крысс, наследующий функции
+    рисования мышей, движения мышей,
+    и отражения мышей от стен
+    """
     def __init__(self, x1, x2, y1, y2):
+        """
+        Вводим диапозон генерации мышей, их скорость, размер и цвет
+        """
         self.r = 30
         self.x = randint(x1, x2)
         self.y = randint(y1, y2)
@@ -94,8 +126,12 @@ class Rat(Mouse):
         self.color = (73, 66, 61)
 
     def touch_mouse(self, i):
+        """
+        Функция лопания мышей
+        """
         global points, k, pop_x, pop_y, n
-        if (self.x - event.pos[0]) ** 2 + (self.y - event.pos[1]) ** 2 <= self.r ** 2:
+        if (self.x - event.pos[0]) ** 2 +\
+                (self.y - event.pos[1]) ** 2 <= self.r ** 2:
             self.r -= 6
             self.speed += 2
             self.vx += sign(self.vx)*randint(15, 20)
@@ -110,8 +146,10 @@ class Rat(Mouse):
 
 
 num_of_rodents_rand = number_of_rodents//6
-number_of_mice = randint(3*num_of_rodents_rand, 4*num_of_rodents_rand)
-number_of_rats = number_of_rodents - number_of_mice
+number_of_mice = randint(3*num_of_rodents_rand,
+                         4*num_of_rodents_rand)  # задаем кол-во мышей
+number_of_rats = number_of_rodents - number_of_mice  # задаем кол-во крыс
+# Заполняем список наших грызунов и рисуем их
 for i in range(number_of_mice):
     rodents.append(Mouse(110, 690, 110, 690))
     rodents[i].draw_mouse()
@@ -127,20 +165,24 @@ while not finished:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             finished = True
-        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-            if (event.pos[0] >= 100) and (event.pos[0] <= 300) and (event.pos[1] >= 710):
+        elif event.type == pg.MOUSEBUTTONDOWN \
+                and event.button == 1:
+            if (event.pos[0] >= 100) and (event.pos[0] <= 300)\
+                    and (event.pos[1] >= 710):
+                # создаем псевдокнопку паузы
                 save_sec = seconds
                 save_min = minutes
                 save_points = points
                 points = -1
             for i in range(len(rodents) - 1, -1, -1):
-                rodents[i].touch_mouse(i)
+                rodents[i].touch_mouse(i) # ловим грызунов
     pg.display.update()
     screen.fill((255, 255, 255))
     rect(screen, (255, 204, 255), (100, 100, 600, 600))
-    font.render_to(screen, (20, 20), "Catpoints: " + str(points), (255, 153, 255))
+    font.render_to(screen, (20, 20), "Catpoints: " +
+                   str(points), (255, 153, 255))  # выводим кол-во очков на экран
     timer += 1
-    if timer == 30:
+    if timer == 30:  # задаем изменеие времени
         seconds -= 1
         timer = 0
     if seconds == 0 and minutes != 0:
@@ -148,41 +190,54 @@ while not finished:
         seconds = 59
     if seconds == 0 and minutes == 0:
         finished = True
-    pg.draw.polygon(screen, (255, 153, 255), ((100, 710), (360, 710), (360, 800), (100, 800)), 10)
+    pg.draw.polygon(screen, (255, 153, 255),
+                    ((100, 710), (360, 710),
+                     (360, 800), (100, 800)), 10)
     font.render_to(screen, (110, 750), "КОТИК УСТАЛ", (255, 153, 255))
-    font.render_to(screen, (510, 20), " ВреМЯУ: " + str(minutes) + ' m ' + str(seconds) + ' s', (255, 153, 255))
+    font.render_to(screen, (510, 20), " ВреМЯУ: " + str(minutes)
+                   + ' m ' + str(seconds) + ' s', (255, 153, 255))  # выводим время в минутах и секундах
     if k < 7:
-        font.render_to(screen, (pop_x, pop_y), '+' + str(n), (255, 153, 255))
+        font.render_to(screen, (pop_x, pop_y), '+' +
+                       str(n), (255, 153, 255))  # выводим кол-во очков за пойманного грызуна на экран
         k += 1
-    for i in range(len(rodents)):
+    for i in range(len(rodents)):  # рисование, движение, отражение грызунов
         rodents[i].draw_mouse()
         rodents[i].move(1)
         rodents[i].reflection()
-    if points == -1:
+    if points == -1:  # псевдоэкран паузы
         screen.fill((255, 255, 255))
-        pg.draw.polygon(screen, (255, 153, 255), ((50, 300), (330, 300), (330, 390), (50, 390)), 10)
+        pg.draw.polygon(screen, (255, 153, 255),
+                        ((50, 300), (330, 300),
+                         (330, 390), (50, 390)), 10)
         font.render_to(screen, (60, 340), "КОТИК УШЕЛ", (255, 153, 255))
-        if (event.type == pg.MOUSEBUTTONDOWN) and (event.button == 1) and (event.pos[0] > 100) and (
-                event.pos[0] < 380) and (event.pos[1] > 300) and (event.pos[1] < 390):
+        if (event.type == pg.MOUSEBUTTONDOWN) and (event.button == 1) \
+                and (event.pos[0] > 100) and (
+                event.pos[0] < 380) and (event.pos[1] > 300)\
+                and (event.pos[1] < 390):  # псевдо кнопка выхода
             points = save_points
             finished = True
-        pg.draw.polygon(screen, (255, 153, 255), ((370, 300), (710, 300), (710, 390), (370, 390)), 10)
-        font.render_to(screen, (380, 340), "КОТИК ОТДОХНУЛ", (255, 153, 255))
-        if (event.type == pg.MOUSEBUTTONDOWN) and (event.button == 1) and (event.pos[0] > 450) and (
-                event.pos[0] < 790) and (event.pos[1] > 300) and (event.pos[1] < 390):
+        pg.draw.polygon(screen, (255, 153, 255),
+                        ((370, 300), (710, 300),
+                         (710, 390), (370, 390)), 10)
+        font.render_to(screen, (380, 340), "КОТИК ОТДОХНУЛ",  # псевдокнопка продолжения игры
+                       (255, 153, 255))
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 \
+                and 790 > event.pos[0] > 450 and 390 > event.pos[1] > 300:
             seconds = save_sec
             minutes = save_min
             points = save_points
             timer = 0
             rest = False
-screen.fill((255, 255, 255))
-font.render_to(screen, (100, 100), "Какая кличка у вашего котика? ", (255, 153, 255))
+screen.fill((255, 255, 255))  # ввод имени игрока
+font.render_to(screen, (100, 100),
+               "Какая кличка у вашего котика? ", (255, 153, 255))
 finished = False
 name = ""
 while finished is False:
     screen.fill((255, 255, 255))
-    font.render_to(screen, (100, 100), "Какая кличка у вашего котика? ", (255, 153, 255))
-    for event in pygame.event.get():
+    font.render_to(screen, (100, 100),
+                   "Какая кличка у вашего котика? ", (255, 153, 255))
+    for event in pygame.event.get():  # вывод написанного на экран
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
                 finished = True
@@ -192,16 +247,17 @@ while finished is False:
                 name += pygame.key.name(event.key)
         font.render_to(screen, (100, 140), name, (255, 153, 255))
         pygame.display.update()
-with open(r"C:\Users\Asus\PycharmProjects\Karpenko_MIPT_Pythonlabs\Catch the ball\record_table.json") as f:
-    data = json.load(f)
+with open(r"C:\Users\Asus\PycharmProjects\Karpenko_MIPT_Pythonlabs\Catch the ball\record_table.json") as record_table:
+    data = json.load(record_table)
 data[name] = points
 screen.fill((255, 255, 255))
 height = 50
-for p, v in data.items():
-    font.render_to(screen, (20, height), "имя котика : " + p + "  его catpoints: " + str(v), (255, 153, 255))
+for p, v in data.items():  # вывод таблицы с игроками и их котоочками
+    font.render_to(screen, (20, height), "имя котика : "
+                   + p + "  его catpoints: " + str(v), (255, 153, 255))
     height += 40
-    if (event.type == pg.MOUSEBUTTONDOWN) and (event.button == 1) and (event.pos[0] > 100) and (
-            event.pos[0] < 380) and (event.pos[1] > 300) and (event.pos[1] < 390):
+    if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and \
+            390 > event.pos[1] > 300 > event.pos[0] > 100:
         finished = True
     pygame.display.update()
     finished = False
@@ -211,7 +267,8 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-with open(r"C:\Users\Asus\PycharmProjects\Karpenko_MIPT_Pythonlabs\Catch the ball\record_table.json", 'w') as f:
-    json.dump(data, f)
+with open(r"C:\Users\Asus\PycharmProjects\Karpenko_MIPT_Pythonlabs\Catch the ball\record_table.json", 'w') as \
+        record_table:  # записываем игрока в файл
+    json.dump(data, record_table)
 
 pg.quit()
